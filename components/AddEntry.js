3648893f661ee34279645e28a643e400181de02d
Sumedha <suma@Sumedhas-MacBook-Pro.local>
2019-05-21 00:32:55 +0530

    Added state change methods

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -3,6 +3,46 @@ import { View } from 'react-native'
 import { getMetricMetaInfo } from '../utils/helpers'
 
 export default class AddEntry extends Component {
+    state = {
+        run: 0,
+        bike: 0,
+        swim: 0,
+        eat: 0,
+        sleep: 0
+    }
+
+    increment = (metric) => {
+        const { max, step } = getMetricMetaInfo(metric)
+
+        this.setState((prevState) => {
+            const count = prevState[metric] + step
+            return {
+                ...prevState,
+                [metric]: count > max ? max : count
+            }
+        })
+    }
+
+    decrement = (metric) => {
+        const { step } = getMetricMetaInfo(metric)
+
+        this.setState((prevState) => {
+            const count = prevState[metric] - step
+            return {
+                ...prevState,
+                [metric]: count < 0 ? 0 : count
+            }
+        })
+    }
+
+    slide = (metric, value) => {
+        this.setState(() => (
+            {
+                [metric]: value
+            }
+        ))
+    }
+
     render() {
         return <View>
             {getMetricMetaInfo('bike').getIcon()}

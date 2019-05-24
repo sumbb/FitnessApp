e8f11d458f255bfbb91fd5481200452aa0f81723
Sumedha Birajdar <sumedhabb1996@gmail.com>
2019-05-24 17:40:09 +0530

    Added submit button

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
-import { getMetricMetaInfo } from '../utils/helpers'
+import { View, TouchableOpacity, Text } from 'react-native'
+import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import Slider from './Slider';
 import Stepper from './Stepper';
 import DateHeader from './DateHeader'
+
+function SubmitBtn({ onPress }) {
+    return (
+        <TouchableOpacity
+         onPress={onPress}>
+             <Text>Submit</Text>
+        </TouchableOpacity>
+    )
+}
 
 export default class AddEntry extends Component {
     state = {
@@ -12,6 +21,24 @@ export default class AddEntry extends Component {
         swim: 0,
         eat: 0,
         sleep: 0
+    }
+    
+    submit = () => {
+        const key = timeToString()
+        const entry = this.state
+
+        this.setState({
+            run: 0,
+            bike: 0,
+            swim: 0,
+            eat: 0,
+            sleep: 0
+        })
+
+        // TODO: update redux
+        // TODO: navigate to home
+        // TODO: save value to database
+        // TODO: clear the notification
     }
 
     increment = (metric) => {
@@ -74,6 +101,7 @@ export default class AddEntry extends Component {
                     </View>
                 )
             })}
+            <SubmitBtn onPress={this.submit}/>
         </View>
     }
 }

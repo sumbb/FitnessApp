59de0d53d63a01851d5664137351683bb7669a48
Sumedha Birajdar <sumedhabb1996@gmail.com>
2019-05-28 17:51:06 +0530

    Added tab navigation

--- App.js
+++ App.js
@@ -1,18 +1,61 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { Component } from 'react'
+import { StyleSheet, Text, View, Platform } from 'react-native'
 import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/History'
+import { createBottomTabNavigator } from 'react-navigation'
+import { purple, white } from './utils/colors'
+import { FontAwesome, Ionicons } from '@expo/vector-icons'
 
-export default class App extends React.Component {
+
+const routeConfig = {
+  History: {
+    screen: History,
+    navigationOptions: {
+      tabBarLabel: 'History',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    },
+  },
+  AddEntry: {
+    screen: AddEntry,
+    navigationOptions: {
+      tabBarLabel: 'Add Entry',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    },
+  }
+}
+
+const bottomTabNavigatorConfig = {
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+}
+
+const Tabs = Platform.OS === 'ios' 
+  ? createBottomTabNavigator(routeConfig, bottomTabNavigatorConfig)
+  : createMaterialTopTabNavigator(routeConfig, bottomTabNavigatorConfig)
+
+
+
+export default class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
-          <View style={{ height: 30}}/>
-          <History />
+          <Tabs/>
         </View>
       </Provider>
     );

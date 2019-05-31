4da3a128e166b2f4fee302c84067eff655ea8b94
Sumedha <sumedhabb1996@gmail.com>
2019-05-31 22:00:15 +0530

    Added the denied page

--- components/Live.js
+++ components/Live.js
@@ -6,12 +6,12 @@ import { purple, white } from '../utils/colors'
 export default class Live extends Component {
     state = {
         coords: null,
-        status: 'undermined',
+        status: 'denied',
         direction: ''
     }
 
     askPermission = () => {
-        
+
     }
 
     render() {
@@ -23,8 +23,9 @@ export default class Live extends Component {
 
         if(status === 'denied') {
             return (
-                <View>
-                    <Text>Denied</Text>
+                <View style={styles.center}>
+                    <Foundation name='alert' size={50}/>
+                    <Text>You denied your location, you can fix this by visiting your settings and enabling location services for this app.</Text>
                 </View>
             )
         }

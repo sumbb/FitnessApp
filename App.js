b0d7b2df4c7f623543730797902e7d5e0e9b15c4
Sumedha <sumedhabb1996@gmail.com>
2019-05-31 21:48:30 +0530

    Added the Live component

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import { purple, white, red } from './utils/colors'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import { Constants } from 'expo'
 import EntryDetail from './components/EntryDetail'
+import Live from './components/Live'
 
 const routeConfig = {
   History: {
@@ -25,7 +26,14 @@ const routeConfig = {
       tabBarLabel: 'Add Entry',
       tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
     },
-  }
+  },
+  Live: {
+    screen: Live,
+    navigationOptions: {
+      tabBarLabel: 'Live',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-speedometer' size={30} color={tintColor} />
+    },
+  },
 }
 
 const bottomTabNavigatorConfig = {

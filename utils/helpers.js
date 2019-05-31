faa4bffd568468260768842043d54c2fa5e13bd2
Sumedha <sumedhabb1996@gmail.com>
2019-05-31 23:29:59 +0530

    Added the local notification helper functions

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, AsyncStorage } from 'react-native';
 import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
 import { red, orange, blue, lightPurp, pink } from './colors'
+import { Notifications, Permissions } from 'expo'
 
 // utils/helpers.js
+
+const NOTIFICATION_KEY = 'Appnotifications'
+
 
 export function isBetween (num, x, y) {
     if (num >= x && num <= y) {
@@ -162,4 +166,55 @@ export function isBetween (num, x, y) {
     return {
       today: "👋 Don't forget to log your data today!"
     }
+  }
+
+  export function clearLocalNotification() {
+    return AsyncStorage.removeItem(NOTIFICATION_KEY)
+    .then(Notifications.cancelAllScheduledNotificationsAsync)
+  }
+
+  function createNotification() {
+    return {
+      title: "Log your stats",
+      body: "👋 Don't forget to log your data today!",
+      ios: {
+        sound: true
+      },
+      android: {
+        sound: true,
+        priority: false,
+        sticky: false,
+        vibrate: true
+      }
+    }
+  }
+
+  export function setLocalNotification() {
+      AsyncStorage.getItem(NOTIFICATION_KEY)
+       .then(JSON.parse)
+       .then((data) => {
+          if(data === null) {
+            Permissions.askAsync(Permissions.NOTIFICATIONS)
+              .then(({ status }) => {
+                 if(status === 'granted') {
+                   Notifications.cancelAllScheduledNotificationsAsync()
+
+                   let tomorrow = new Date()
+                    tomorrow.setDate(tomorrow.getDate() + 1)
+                    tomorrow.setHours(20)
+                    tomorrow.setMinutes(0)
+
+                    Notifications.scheduleLocalNotificationAsync(
+                      createNotification(),
+                      {
+                        time: tomorrow,
+                        repeat: 'day',
+                      }
+                    )
+      
+                    AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true))
+                 }   
+              })  
+          }
+       })
   }
